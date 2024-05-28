c5348fa9c7419a6adc0ca61c09f2e22c9cd4c4f6
abdn123 <abdullahnaeem802>
2024-05-28 21:01:24 +0500

    updated test

--- tests/test_script.py
+++ tests/test_script.py
@@ -3,19 +3,9 @@ from selenium.webdriver.common.by import By
 import time
 
 # URL of the first application
-url_app1 = "http://<app1-url>"
+url_app2 = "http://localhost:80"
 
-# URL of the second application
-url_app2 = "http://<app2-url>"
 
-# Function to run tests for App 1
-def test_app1():
-    driver = webdriver.Chrome()
-    driver.get(url_app1)
-    time.sleep(2)  # Wait for 2 seconds for the page to load
-    assert "App 1" in driver.title  # Assert that "App 1" is in the title
-    print("Test for App 1 passed!")
-    driver.quit()
 
 # Function to run tests for App 2
 def test_app2():
@@ -26,6 +16,5 @@ def test_app2():
     print("Test for App 2 passed!")
     driver.quit()
 
-# Run tests for both applications
-test_app1()
+
 test_app2()
